e39f3785308a600aa2dcaddcdfb504e950766016
Prashant-Sharma-2056 <prashantsharma3375@gmail.com>
2026-05-24 12:45:07 +0530

    Day-3 uploaded

--- Day-3/09.c
+++ Day-3/09.c
@@ -1,25 +1,20 @@
 // Write a program to check wheather a number is Prime.
 #include <stdio.h>
 
-int checkPrime(int number){
-    int status = 1;
-
-    for (int i = 2; i < number; i++){
-        if (number % i == 0){
-            status = 0;
-            break;
-        }
-    }
-
-    return status;
-}
-
 int main(){
     int num;
     printf("Enter the number: \n");
     scanf("%d", &num);
 
-    if (checkPrime(num) == 1) printf("number is PRIME");
+    int status = 1;
+    for (int i = 2; i < num; i++){
+        if (num % i == 0){
+            status = 0;
+            break;
+        }
+    }
+
+    if (status) printf("number is PRIME");
     else printf("number is NOT prime");
 
     return 0;
--- Day-3/10.c
+++ Day-3/10.c
@@ -1,19 +1,6 @@
 // Write a Program to print prime number in a range.
 #include <stdio.h>
 
-int checkPrime(int number){
-    int status = 1;
-
-    for (int i = 2; i < number; i++){
-        if (number % i == 0){
-            status = 0;
-            break;
-        }
-    }
-
-    return status;
-}
-
 int main(){
     int start, end;
     printf("Set the range to print prime number by entering the starting and ending number (Eg: 10, 100): \n");
@@ -21,7 +8,16 @@ int main(){
 
     printf("Prime numbers between %d and %d are: \n", start, end);
     for (int i = start; i <= end; i++){
-        if (checkPrime(i) == 1) printf("%d\n", i);
+        int status = 1;
+
+        for (int j = 2; j < i; j++){
+            if (i % j == 0){
+                status = 0;
+                break;
+            }
+        }
+
+        if (status) printf("%d\n", i);
     }
 
     return 0;
