c36a3f8444c7f36e21e41571e113d18e38a5bfa0
Prashant-Sharma-2056 <prashantsharma3375@gmail.com>
2026-06-01 19:31:17 +0530

    Day-10 uploaded

---
+++ Day-10/37.c
@@ -0,0 +1,22 @@
+// Write a program to print star pyramid
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the value of n: \n");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < (n - i - 1); j++){
+            printf("  ");
+        }
+
+        for (int j = 0; j < (2 * i + 1); j++){
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Day-10/40.c
@@ -0,0 +1,25 @@
+// Write a program to print character pyramid
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the value of n: \n");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++){
+        for (int j = 0; j < n - i; j++){
+            printf("  ");
+        }
+
+        for (int j = 1; j <= i; j++){
+            printf("%c ", j + 64);
+        }
+
+        for (int j = i - 1; j >= 1; j--){
+            printf("%c ", j + 64);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
